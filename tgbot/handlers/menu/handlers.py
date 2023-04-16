18ea6d7d7184acd233e043b05b064eccdf04545a
Kostiantyn Karmeliuk <kostiantyn.karmeliuk@coaxsoft.com>
2023-04-16 20:51:39 +0300

    Send menu after sale creation

--- tgbot/handlers/menu/handlers.py
+++ tgbot/handlers/menu/handlers.py
@@ -7,7 +7,14 @@ from tgbot.handlers.onboarding.static_text import greeting_text
 
 def callback_menu(update: Update, context: CallbackContext) -> None:
     keyboard = make_menu_keyboard()
-    update.callback_query.edit_message_text(
-        greeting_text,
-        reply_markup=keyboard
-    )
+    if update.callback_query:
+        update.callback_query.edit_message_text(
+            greeting_text,
+            reply_markup=keyboard
+        )
+    else:
+        context.bot.send_message(
+            update.effective_chat.id,
+            greeting_text,
+            reply_markup=keyboard
+        )
--- tgbot/handlers/sale_creation/create_sale/handlers.py
+++ tgbot/handlers/sale_creation/create_sale/handlers.py
@@ -2,6 +2,7 @@ from telegram import Update, ParseMode
 from telegram.ext import CallbackContext
 
 from sales.models import SalePlacement, CompanyAccount
+from tgbot.handlers.menu.handlers import callback_menu
 from tgbot.handlers.sale_creation.create_sale.keyboards import make_sales_preview_keyboard, create_new_sale_keyboard
 from tgbot.handlers.sale_creation.create_sale import static_text
 
@@ -11,16 +12,16 @@ def callback_confirm_sale_creation(update: Update, context: CallbackContext) -> 
         update.effective_chat.id,
         context.user_data
     )
-    print(context.user_data)
     sale.save()
     sale_text = sale.generate_sale_text()
-    keyboard = create_new_sale_keyboard()
     # Send sale
     update.callback_query.edit_message_text(
         sale_text,
-        reply_markup=keyboard,
         parse_mode=ParseMode.HTML
     )
+    # Force to send message
+    update.callback_query = None
+    callback_menu(update, context)
 
 
 def callback_create_sales_preview(update: Update, context: CallbackContext) -> None:
