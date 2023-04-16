e4edc706c93c49744bd2378f9ebb0832dba8cfc7
Kostiantyn Karmeliuk <kostiantyn.karmeliuk@coaxsoft.com>
2023-04-16 21:20:40 +0300

    Fix steps in sale creation

--- tgbot/handlers/sale_creation/currency/utils.py
+++ tgbot/handlers/sale_creation/currency/utils.py
@@ -1,4 +1,4 @@
-from tgbot.handlers.sale_creation.price.manage_data import INPUT_PRICE_CALLBACK
+from tgbot.handlers.sale_creation.basis.manage_data import INPUT_BASIS_CALLBACK
 from tgbot.handlers.sale_creation.currency.manage_data import CHOSEN_CURRENCY_CALLBACK
 from tgbot.handlers.utils.static_text import callback_separator
 
@@ -8,4 +8,4 @@ def get_chosen_currency_callback_data(currency: str) -> str:
 
 
 def get_go_back_from_choose_currency_callback_data() -> str:
-    return f"{INPUT_PRICE_CALLBACK}"
+    return f"{INPUT_BASIS_CALLBACK}"
